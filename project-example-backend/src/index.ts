ca03a5604e6d5f97c4b70b5611a40f5d368b6480
Muhammad Muaz <muazgill435@gmail.com>
2024-06-10 03:20:34 +0500

    graphql project backend setup

---
+++ project-example-backend/src/index.ts
@@ -0,0 +1,32 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = Number(process.env.PORT as String) || 8000;
+
+async function start() {
+  const app = express();
+
+  app.use(express.json()); 
+
+  const graphql = new ApolloServer({
+    typeDefs: `
+		type Query {
+			hello: String
+		}
+	`,
+    resolvers: {},
+  });
+
+  await graphql.start(); 
+
+  app.use('/graphql', expressMiddleware(graphql)); 
+
+  app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+  });
+}
+
+start(); 
